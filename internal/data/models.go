38ef90eb42f3c4dc4e9839a540b630200a713b11
volcanoeyes <130888597+volcanoeyes@users.noreply.github.com>
2024-03-16 11:35:51 +0800

    first commit

---
+++ internal/data/models.go
@@ -0,0 +1,25 @@
+package data
+
+import (
+	"database/sql"
+	"errors"
+)
+
+var (
+	ErrRecordNotFound = errors.New("record not found")
+	ErrEditConflict   = errors.New("edit conflict")
+)
+
+type Models struct {
+	Movies MovieModel
+	Users  UserModel
+	Tokens TokenModel
+}
+
+func NewModels(db *sql.DB) Models {
+	return Models{ // note the flexibility of having different databases
+		Movies: MovieModel{DB: db},
+		Users:  UserModel{DB: db},
+		Tokens: TokenModel{DB: db},
+	}
+}
---
+++ internal/data/runtime.go
@@ -0,0 +1,42 @@
+package data
+
+import (
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+var ErrInvalidRuntimeFormat = errors.New("invalid runtime format")
+
+type Runtime int32 // be aware other packages may have a harder time using custom types
+
+func (r Runtime) MarshalJSON() ([]byte, error) {
+	jsonValue := fmt.Sprintf("%d mins", r)
+
+	// want JSON string, so must have double quotes
+	quotedJSONValue := strconv.Quote(jsonValue)
+
+	return []byte(quotedJSONValue), nil
+}
+
+func (r *Runtime) UnmarshalJSON(jsonValue []byte) error {
+	unquotedJSONValue, err := strconv.Unquote(string(jsonValue))
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	parts := strings.Split(unquotedJSONValue, " ")
+
+	if len(parts) != 2 || parts[1] != "mins" {
+		return ErrInvalidRuntimeFormat
+	}
+
+	i, err := strconv.ParseInt(parts[0], 10, 32)
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+	*r = Runtime(i)
+
+	return nil
+}
